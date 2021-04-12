d093c12613134a6c08725f23215febc4e7d05793
Bella Patel <bpate54@uic.edu>
2021-04-12 16:36:33 -0500

    Update final project.R

--- final project.R
+++ final project.R
@@ -5,15 +5,16 @@ second <- read.csv("Dsecond.csv")
 third <- read.csv("Dthird.csv")
 
 unique(first$target) # still, car, train, bus, walking
+unique(as.integer(first$target)) # 3 2 4 1 5
 
 colnames(first)
 colnames(second)
 colnames(third)
 
-length(unique(third$speed.max))
-length(third$speed.max)
-
-unique(first$target) # still, car, train, bus, walking
+cor(as.integer(first$target), first$android.sensor.accelerometer.mean) # .390
+cor(as.integer(first$target), first$android.sensor.gyroscope.mean) # .458
+cor(as.integer(first$target), first$sound.mean) # .01
+cor(as.integer(first$target), first$android.sensor.gyroscope.max)
 
 # logistic regression
 # support vector machine
