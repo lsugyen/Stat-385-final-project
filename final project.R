78ed94383ea1f3b143603a00ce69f9d68b5ab4b8
bellapatel <82414275+bellapatel@users.noreply.github.com>
2021-04-14 18:30:23 -0500

    Update final project.R

--- final project.R
+++ final project.R
@@ -19,16 +19,6 @@ plot(total$target, total$speed.mean)
 
 
 # logistic regression
-gyroscope <-as.double(total[,7]) # android.sensor.gyroscope.mean
-acceleration <-as.double(total[,23]) # android.sensor.linear_acceleration.mean
-target <- as.factor(total[,14])
-logitreg <-glm(target~gyroscope+acceleration,family="binomial")
-summary(logitreg)
-with(summary(logitreg), 1 - deviance/null.deviance)
-# measure the ratio of the deviance in your model to the null; R-squared is "goodness of fit"; how much of your data is explained by the data.
-
-
-# logistic regression
 # support vector machine
 # classificiation trees
 # k means / k nearest neighborhoods
