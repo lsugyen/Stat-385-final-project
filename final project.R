4c013f3e714b04f3895d0be94db26a8886c645d6
bellapatel <82414275+bellapatel@users.noreply.github.com>
2021-04-15 17:06:10 -0500

    Update final project.R

--- final project.R
+++ final project.R
@@ -1,22 +1,88 @@
-setwd("/Users/bellapatel/Documents/GitHub/Stat-385-final-project")
+setwd("/Users/bellapatel/stat385/")
 
-first <- read.csv("Dfirst.csv")
-second <- read.csv("Dsecond.csv")
-third <- read.csv("Dthird.csv")
+normal <- read.csv("max_normal.csv")
 
-unique(first$target) # still, car, train, bus, walking
-unique(as.integer(first$target)) # 3 2 4 1 5
+colnames(normal)
+normal$target
 
-colnames(first)
-colnames(second)
-colnames(third)
+gyroscope <-as.double(normal[,3]) # android.sensor.gyroscope.mean
+acceleration <-as.double(normal[,2]) # android.sensor.linear_acceleration.mean
+speed <- as.double(normal[,11]) # speed.mean
 
-cor(as.integer(first$target), first$android.sensor.accelerometer.mean) # .390
-cor(as.integer(first$target), first$android.sensor.gyroscope.mean) # .458
-cor(as.integer(first$target), first$sound.mean) # .01
-cor(as.integer(first$target), first$android.sensor.gyroscope.max)
 
-# logistic regression (Bella)
-# support vector machine (Stephanie)
-# clafficiation trees (Albert)
-# k means / k nearest neighborhoods (Albert)
+
+
+# combine several classes into two classes, then apply binary classification iteratively
+# create binary target
+
+# bus
+normal <- read.csv("max_normal.csv")
+normal$target <- as.character(normal$target)
+normal$target[normal$target != "Bus"] <- 0
+normal$target[normal$target == "Bus"] <- 1
+normal$target <- as.integer(normal$target)
+target <- as.integer(normal[,5])
+
+logisticbus <-glm(target~acceleration+speed+gyroscope, family="binomial") #repeat for all targets i guess
+# coef(logisticbus)
+with(summary(logisticbus), 1 - deviance/null.deviance)
+
+# predict(logisticbus, newdata=data.frame(acceleration=.24, gyroscope=.028, speed = .39), type="response")
+
+# train
+normal <- read.csv("max_normal.csv")
+normal$target <- as.character(normal$target)
+normal$target[normal$target != "Train"] <- 0
+normal$target[normal$target == "Train"] <- 1
+normal$target <- as.integer(normal$target)
+target <- as.integer(normal[,5])
+
+logistictrain <-glm(target~acceleration+speed+gyroscope, family="binomial") #repeat for all targets i guess
+# coef(logistictrain)
+with(summary(logistictrain), 1 - deviance/null.deviance)
+
+# predict(logistictrain, newdata=data.frame(acceleration=.24, gyroscope=.028, speed = .39), type="response")
+
+# car
+normal <- read.csv("max_normal.csv")
+normal$target <- as.character(normal$target)
+normal$target[normal$target != "Car"] <- 0
+normal$target[normal$target == "Car"] <- 1
+normal$target <- as.integer(normal$target)
+target <- as.integer(normal[,5])
+
+logisticcar <-glm(target~acceleration+speed+gyroscope, family="binomial") #repeat for all targets i guess
+# coef(logisticcar)
+with(summary(logisticcar), 1 - deviance/null.deviance)
+
+# predict(logisticcar, newdata=data.frame(acceleration=.24, gyroscope=.028, speed = .39), type="response")
+
+# still
+normal <- read.csv("max_normal.csv")
+normal$target <- as.character(normal$target)
+normal$target[normal$target != "Still"] <- 0
+normal$target[normal$target == "Still"] <- 1
+normal$target <- as.integer(normal$target)
+target <- as.integer(normal[,5])
+
+logisticstill <-glm(target~acceleration+speed+gyroscope, family="binomial") #repeat for all targets i guess
+# coef(logisticstill)
+with(summary(logisticstill), 1 - deviance/null.deviance)
+
+# predict(logisticstill, newdata=data.frame(acceleration=0, gyroscope=0, speed = 0), type="response")
+
+# walking
+normal <- read.csv("max_normal.csv")
+normal$target <- as.character(normal$target)
+normal$target[normal$target != "Walking"] <- 0
+normal$target[normal$target == "Walking"] <- 1
+normal$target <- as.integer(normal$target)
+target <- as.integer(normal[,5])
+
+logisticwalking <-glm(target~acceleration+speed+gyroscope, family="binomial") #repeat for all targets i guess
+# coef(logisticwalking)
+with(summary(logisticwalking), 1 - deviance/null.deviance)
+
+# predict(logisticwalking, newdata=data.frame(acceleration=.3, gyroscope=.5, speed = .5), type="response")
+
+
